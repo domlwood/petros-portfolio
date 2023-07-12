dd11a6d352278b9077321c5a65f4685897a9e9fc
Dominic Wood <dominic.wood@theapsgroup.com>
2023-07-12 22:22:42 +0100

    chore: increase intro size

--- app/main/features/Intro.tsx
+++ app/main/features/Intro.tsx
@@ -2,7 +2,7 @@ export const Intro = () => {
   return (
     <section className="flex justify-center items-center">
       <h1 className="w-full">
-        Hi, I'm <span className="text-underline">Petros</span>,
+        Hi, I'm <span className="underline underline-offset-8">Petros</span>
         <br />
         I’m an interaction designer, <br />a prototyper, a maker and <br />
         an aspiring design writer. <br />I develop rich meaningful <br />
