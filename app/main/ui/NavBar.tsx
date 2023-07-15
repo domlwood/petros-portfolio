901b0e2d851e50bd425aaa3c5eff5f34532734be
Dominic Wood <dominic.wood@theapsgroup.com>
2023-07-15 13:25:42 +0100

    fix: window is not defined

--- app/main/ui/NavBar.tsx
+++ app/main/ui/NavBar.tsx
@@ -1,11 +1,12 @@
-'use client';
+'use client'
+
 import Link from 'next/link';
 import Image from 'next/image';
 import { useState } from 'react';
 
 export const NavBar = () => {
   const [open, setOpen] = useState(false);
-  const currentLink = window.location.hash
+  const currentLink = window?.location?.hash;
   const links = [
     {
       name: 'about',
@@ -32,13 +33,20 @@ export const NavBar = () => {
   return open ? (
     <div className="fixed flex flex-col justify-between w-screen h-screen text-right backdrop-blur bg-black/60">
       <div>
-      <Image className="ml-auto" alt="" width={132} height={132} onClick={() => setOpen(false)} src="/close-nav.svg"/>
-      <div className="px-[100px]">
-        <h2 className="pb-5">Petros Chantzopoulos</h2>
-        <p className="leading-10">petros.chantz [at] gmail [dot] com</p>
-        <p className="leading-10">+31 XXXXXXXX XXX</p>
-        <a className="leading-10">LinkedIn</a>
-      </div>
+        <Image
+          className="ml-auto"
+          alt=""
+          width={132}
+          height={132}
+          onClick={() => setOpen(false)}
+          src="/close-nav.svg"
+        />
+        <div className="px-[100px]">
+          <h2 className="pb-5">Petros Chantzopoulos</h2>
+          <p className="leading-10">petros.chantz [at] gmail [dot] com</p>
+          <p className="leading-10">+31 XXXXXXXX XXX</p>
+          <a className="leading-10">LinkedIn</a>
+        </div>
       </div>
       <div className="p-[100px]">
         {links.map((link) => (
@@ -50,7 +58,7 @@ export const NavBar = () => {
     </div>
   ) : (
     <div className="fixed top-0 right-0 text-right">
-      <Image width={132} height={132} alt="" onClick={() => setOpen(true)} src="/open-nav.svg"/>
+      <Image width={132} height={132} alt="" onClick={() => setOpen(true)} src="/open-nav.svg" />
     </div>
   );
 };
